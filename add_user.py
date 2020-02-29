9bee84306280ba59dc555477e80c08800c5bee6e
Connor <ConnorRadeloff1@gmail.com>
2020-02-29 14:26:02 -0700

    Added salt and hash to add_user.py

--- add_user.py
+++ add_user.py
@@ -5,13 +5,15 @@
     Authors: Matt Niemiec and Abigail Fernandes
     The solution contains the same number of lines (plus imports)
 """
+import os
+import hashlib
 
 user = input("Enter a username: ")
 password = input("Enter a password: ")
 
 # TODO: Create a salt and hash the password
-salt =
-hashed_password =
+salt = os.urandom(16)
+hashed_password = hashlib.pbkdf2_hmac('sha256', password.encode(), salt, 100000)
 
 try:
     reading = open("passfile.txt", 'r')
